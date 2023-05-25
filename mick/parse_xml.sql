06060b7e169ed679a193a1328a9c8e6ee72a7905
Ranjith Vijayan <cv.ranjith@gmail.com>
2023-05-25 15:50:47 +0800

    Update parse_xml.sql

--- mick/parse_xml.sql
+++ mick/parse_xml.sql
@@ -5,6 +5,33 @@ PROCEDURE parse_xml(p_xml_string IN VARCHAR2) IS
   l_node XMLDOM.DOMNode;
   l_tag_name VARCHAR2(400);
   l_value VARCHAR2(4000);
+  function fn_get_xpath(n XMLDOM.DOMNode) return VARCHAR2
+  is
+    l_pth varchar2(4000);
+    n1 XMLDOM.DOMNode;
+    l_pth1 varchar2(4000);
+    tb dbms_sql.varchar2_table;
+  BEGIN
+    n1 := n;
+    for i in 1..100
+    LOOP
+        l_pth1 := XMLDOM.getNodeName(n1);
+        if l_pth1 is null or l_pth1 = '#document' 
+        then
+            exit;
+        end if;
+        tb(tb.count+1):=l_pth1;
+        n1 := XMLDOM.GETPARENTNODE(n1);
+    end loop;
+    if tb.count > 0
+    then
+        for i in reverse tb.first..tb.last
+        loop
+            l_pth := l_pth|| '/' || tb(i);
+        end loop;
+    end if;
+    return l_pth;
+  end;
 BEGIN
   l_doc := XMLDOM.newDOMDocument(p_xml_string);
   l_node_list := XMLDOM.getElementsByTagName(l_doc, '*');
@@ -12,7 +39,7 @@ BEGIN
     l_node := XMLDOM.item(l_node_list, i);
     l_tag_name := XMLDOM.getNodeName(l_node);
     l_value := xmldom.getnodevalue(xmldom.getfirstchild(l_node));
-    DBMS_OUTPUT.put_line(l_tag_name || ' = ' || l_value);
+    DBMS_OUTPUT.put_line(fn_get_xpath(l_node) || ' = ' || l_value);
   END LOOP;
 END;
 BEGIN
@@ -41,3 +68,5 @@ BEGIN
 </Document>');
 end;
 /
+
+
